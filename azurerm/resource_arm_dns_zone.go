d229723859bdb21011cc000c0857e45bb8f647a7
Matt White <matt.ffffff@gmail.com>
2019-12-02 10:34:02 +0000

    Update DNS SDK to 2018-05-01

--- azurerm/resource_arm_dns_zone.go
+++ azurerm/resource_arm_dns_zone.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/preview/dns/mgmt/2018-03-01-preview/dns"
 	"github.com/hashicorp/go-azure-helpers/response"
+	"github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2018-05-01/dns"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/azure"
